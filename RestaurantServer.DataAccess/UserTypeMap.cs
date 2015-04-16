3d4228bf1d26f5cd1b0ddabbedee9532c57e119f
ahancock1 <adam.hancock@moov.tv>
2015-04-16 14:18:43 +0100

    Initial commit
    Server for the restaurant app. First commit

---
+++ RestaurantServer.DataAccess/UserTypeMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestuarantServer.Data;
+
+namespace RestaurantServer.DataAccess
+{
+    public class UserTypeMap : EntityTypeConfiguration<UserType>
+    {
+        public UserTypeMap()
+        {
+            Property(u => u.Name).HasMaxLength(12).IsRequired();
+        }
+
+
+    }
+}
---
+++ RestuarantServer.Data/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestuarantServer.Data
+{
+    public class User
+    {
+        public int UserID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+//        [StringLength(12)]
+        public string PhoneNumber { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public UserType UserType { get; set; }
+
+        public User()
+        {
+            
+        }
+    }
+}
