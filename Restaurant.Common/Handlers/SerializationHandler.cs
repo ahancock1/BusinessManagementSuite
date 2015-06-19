cec07abeb756776bdc6805ee4457c0d70978ff05
Adam Hancock <adam.hancock@moov.tv>
2015-06-19 15:08:05 +0100

    Updated WriteToFile serialization
    Tidied
    Directory will be created
    .xml will be added as default extension if there isn't one
    return bool value

--- Restaurant.Common/Handlers/SerializationHandler.cs
+++ Restaurant.Common/Handlers/SerializationHandler.cs
@@ -65,21 +65,54 @@ namespace Restaurant.Common.Handlers
             return default(T);
         }
 
-        public static void WriteFile<T>(this T data, string filePath)
+        public static bool WriteToFile<T>(this T data, string filePath)
         {
-            FileStream stream = new FileStream(filePath, FileMode.Create);
-            XmlDictionaryWriter writer = XmlDictionaryWriter.CreateTextWriter(stream);
-
-            DataContractSerializer serializer = new DataContractSerializer(typeof(T));
-            serializer.WriteObject(writer, data);
-
-            writer.Close();
-            stream.Close();
-
-
             
+            if (!(Attribute.IsDefined(typeof(T), typeof(DataContractAttribute))
+                || Attribute.IsDefined(typeof(T), typeof(SerializableAttribute))))
+                return false;
+            
+            try
+            {
+                if (String.IsNullOrEmpty(Path.GetFileName(filePath)))
+                {
+                    return false;
+                }
+                if (String.IsNullOrEmpty(Path.GetExtension(filePath)))
+                {
+                    filePath += ".xml";
+                }
+                if (!Directory.Exists(filePath))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                    }
+                    catch (Exception e)
+                    {
+                        // Couldn't create directory
+                        return false;
+                    }
+                }
+
+                using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                {
+                    using (XmlDictionaryWriter writer = XmlDictionaryWriter.CreateTextWriter(fs))
+                    {
+                        DataContractSerializer serializer =
+                            new DataContractSerializer(typeof(T));
+                        serializer.WriteObject(writer, data);
+                        return true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Something in the class is not serializable
+
+            }
+
+            return false;
         }
-
-
     }
 }
