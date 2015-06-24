5eb3fa7d0d53ded45e6e94e254dea20343ed4970
ahancock1 <adam.hancock@moov.tv>
2015-06-25 00:18:10 +0100

    Update
    Server can now be hosted as a console app or a windows service depending
    on installation type.
    Only one instance of the server can be running at any time

--- Restaurant.Server/Program.cs
+++ Restaurant.Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
 using System.Threading;
-using Restaurant.Data;
 using Restaurant.Data.Management;
 using Restaurant.Data.Management.Floor;
 using Restaurant.Data.Management.Staff;
@@ -8,25 +9,89 @@ using Restaurant.Listeners;
 
 namespace Restaurant.Server
 {
-    class Program
+    public static class Program
     {
+        #region Nested classes to support running as service
+        private const string ServiceName = "Restaurant Service";
+
         private static Network.Server server;
+
+        private class Service : ServiceBase
+        {
+            public Service()
+            {
+                ServiceName = Program.ServiceName;
+            }
+
+            protected override void OnStart(string[] args)
+            {
+                Program.Start(args);
+            }
+
+            protected override void OnStop()
+            {
+                Program.Stop();
+            }
+        }
+        #endregion
 
         static void Main(string[] args)
         {
+            if (!Environment.UserInteractive)
+                // running as windows service
+                using (var service = new Service())
+                    ServiceBase.Run(service);
+            else
+            {
+                try
+                {
+                    string process = Process.GetCurrentProcess().ProcessName;
+                    Process[] processes = Process.GetProcessesByName(process);
+                    if (processes.Length <= 1)
+                    {
+                        // running as console application
+                        Start(args);
+
+                        Console.WriteLine("Press any key to stop...");
+                        Console.ReadKey(true);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("General failure: {0} \r\n Press any key to stop...", e.Message);
+                    Console.ReadKey(true);
+                }
+
+                Stop();
+            }
+        }
+
+
+        private static void Start(string[] args)
+        {
             // Initiate and start the server
             server = new Network.Server(7777);
-            
+
             // New listener
             server.AddListener(new GenericPacketHandler<StaffMember>());
             server.AddListener(new GenericPacketHandler<Member>());
             server.AddListener(new GenericPacketHandler<Reservation>());
             server.AddListener(new GenericPacketHandler<Shift>());
 
-            
+
             new Thread(server.Start) { Name = "Server" }.Start();
 
-            Console.ReadKey();
+        }
+
+        private static void Stop()
+        {
+            // onstop code here
+            if (server != null)
+            {
+                server.Stop();
+            }
+
+            Environment.Exit(1);  //console app
         }
     }
 }
