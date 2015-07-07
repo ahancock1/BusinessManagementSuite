af4642cf84a53c8593e7a848d50d9f5a2d7295cd
Adam Hancock <adam.hancock@moov.tv>
2015-07-07 19:43:08 +0100

    Update User.cs

--- Restaurant.Data/User.cs
+++ Restaurant.Data/User.cs
@@ -10,31 +10,29 @@ namespace Restaurant.Data
     {
         public int UserID { get; set; }
 
-        [DataMember]
-        [Required(ErrorMessage = "First name is required")]
+        [DataMember, Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
 
-        [DataMember]
-        [Required(ErrorMessage = "Last name is required")]
+        [DataMember, Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
 
-        [DataMember]
-        [Required(ErrorMessage = "Username is required")]
+        [DataMember, Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
 
-        [DataMember]
-        [Required(ErrorMessage = "Date hired is required")]
+        [DataMember, Required(ErrorMessage = "Date hired is required")]
         public DateTime DateHired { get; set; }
 
-        [DataMember]
-        [Required(ErrorMessage = "Phone number is required")]
+        [DataMember, Required(ErrorMessage = "Phone number is required")]
         public string PhoneNumber { get; set; }
 
-        [DataMember]
-        [Required(ErrorMessage = "Email address is required")]
+        [DataMember, Required(ErrorMessage = "Email address is required")]
         public string Email { get; set; }
 
         public virtual UserCredential Credential { get; set; }
+
+        public virtual Restaurant Restaurant { get; set; }
+
+        public virtual Account Account { get; set; }
 
         public User()
         {
