6e112ec5c460837eb7f6b01135edff216c8927b4
Adam Hancock <a.hancock@hotmail.co.uk>
2015-11-26 17:44:44 +0000

    Create AddPasswordResponse.cs

---
+++ Com.Framework.Services/Messages/Membership/AddPasswordResponse.cs
@@ -0,0 +1,9 @@
+using System.Runtime.Serialization;
+
+namespace Com.Framework.Services.Messages.Membership
+{
+    [DataContract]
+    public class AddPasswordResponse : BaseResponse
+    {
+    }
+}
