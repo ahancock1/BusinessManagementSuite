152f4353e20c8e96ca63bfabf1b5a18e976399ee
Adam Hancock <a.hancock@hotmail.co.uk>
2015-11-27 12:49:24 +0000

    Update IGenericService.cs

--- Com.Framework.DataAccess/Services/IGenericService.cs
+++ Com.Framework.DataAccess/Services/IGenericService.cs
@@ -13,20 +13,20 @@ namespace Com.Framework.DataAccess.Services
     {
         [OperationContract]
         IEnumerable<T> All<T>(Func<T, bool> where, params Expression<Func<T, object>>[] include)
-            where T : BaseEntity;
+            where T : class, IBaseEntity;
 
         [OperationContract]
         IPagedList<T> AllPaginated<T>(int page, int pagesize, Func<T, bool> where, params Expression<Func<T, object>>[] include)
-            where T : BaseEntity;
+            where T : class, IBaseEntity;
 
         [OperationContract]
         T Get<T>(Func<T, bool> where, params Expression<Func<T, object>>[] include)
-            where T : BaseEntity;
+            where T : class, IBaseEntity;
 
         [OperationContract]
-        bool Update<T>(params T[] items) where T : BaseEntity;
+        bool Update<T>(params T[] items) where T : class, IBaseEntity;
 
         [OperationContract]
-        Task<bool> UpdateAsync<T>(params T[] items) where T : BaseEntity;
+        Task<bool> UpdateAsync<T>(params T[] items) where T : class, IBaseEntity;
     }
 }
